028eae6db2cceb46e9f1feedc872e7d4600c6684
Matthew Alcasabas <malcasabas28@icloud.com>
2024-11-13 13:53:04 -0800

    added more boilerplate and comments

---
+++ Casino.java
@@ -0,0 +1,10 @@
+public class Casino {
+    /*
+     * this creates a new Casino
+     * Class should:
+     *  - create a new Player
+     *  - keep track of Player's chips
+     *  - keep track of Player's wins/losses
+     *  - manage games
+     */
+}
---
+++ CasinoWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CasinoWindow {
+    /*
+     * this creates and manages the Casino GUI
+     * all GUI management should be done here ideally, including:
+     *  - displaying main menu
+     *  - displaying games list (blackjack, roulette, craps, etc?)
+     *      - obviously we can add/remove games according to time
+     * to keep things organized, each game should be on its own pane, and each pane should be its own file
+     *  - e.g. Blackjack.java, Roulette.java, Craps.java, etc.
+     *      - should we break up game logic and game GUI into different files?
+     */
+}
